3aaf5a2190014339e0cfa6e171a7c882669775fa
Miguel Miranda <lmbasmiranda@gmail.com>
2024-09-26 16:46:48 -0700

    added new challenge

--- 7kyu/sum-of-two-lowest-positive-integers.js
+++ 7kyu/sum-of-two-lowest-positive-integers.js
@@ -11,17 +11,6 @@
 
 function sumTwoSmallestNumbers(numbers) {  
     //Code here
-    let lowest
-    let secondLowest
-  for(let i = 2; i <= numbers.length; i++){
-    lowest = numbers[0];
-    secondLowest = numbers[1];
-    if(numbers[i] < lowest){
-        secondLowest = lowest
-        lowest = numbers[i]
-    } else if(numbers[i] < secondLowest){
-        secondLowest = numbers[i]
-    }
-  }
-  return lowest + secondLowest
-  }
+    numbers.sort((a,b) => a-b)
+    return numbers[0] + numbers[1]
+}
