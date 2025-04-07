68cc323252c9719faf0edc30079fa6be17611dcd
Miguel Miranda <lmbasmiranda@gmail.com>
2025-04-07 11:26:18 +0100

    added new challenge

--- 7kyu/linkedListsPushBuildOneTwoThree.js
+++ 7kyu/linkedListsPushBuildOneTwoThree.js
@@ -19,10 +19,17 @@ function Node(data) {
     this.next = null;
   }
   
+  
   function push(head, data) {
-    // Go.
+    let newNode = new Node(data);
+    newNode.next = head;
+    return newNode;
   }
   
   function buildOneTwoThree() {
-    // Go.
+    let chained = null;
+    chained = push(chained,3);
+    chained = push(chained,2);
+    chained = push(chained,1);
+    return chained;
   }
